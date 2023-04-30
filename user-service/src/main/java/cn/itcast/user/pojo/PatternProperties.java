17a09bffa7219470b7c4801d9c7bddc46091a5ab
jia-Tu <108558126+jia-Tu@users.noreply.github.com>
2023-04-30 15:24:48 +0800

    增加注释

--- user-service/src/main/java/cn/itcast/user/pojo/PatternProperties.java
+++ user-service/src/main/java/cn/itcast/user/pojo/PatternProperties.java
@@ -3,7 +3,7 @@ package cn.itcast.user.pojo;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-
+//时间对象···
 @Data
 @Component
 @ConfigurationProperties(prefix = "pattern")
